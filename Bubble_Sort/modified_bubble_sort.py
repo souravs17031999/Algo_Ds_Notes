80424861ace54eed30619cdaa6ce9e9e825088bc
sourav kumar <souravs_1999@rediffmail.com>
2019-03-06 11:23:11 +0530

    update

--- Bubble_Sort/modified_bubble_sort.py
+++ Bubble_Sort/modified_bubble_sort.py
@@ -1,14 +1,29 @@
-# function for bubble sort
-def Bubble_Sort(list):
-    for i in range(0, len(list) - 1):
-        flag = 0   # flag for tracking the swap so that if transversal over entire array is needed or not 
-        for j in range(0, len(list) - i - 1):
-            # do swapping
-            if list[j] > list[j + 1]:
-                list[j], list[j + 1] = list[j + 1], list[j]
-                flag = 1   
-        if flag == 0:   # if this is true , then it means that no swapping has been done in either the first pass or any other pass , break out of loop
-            break
-
-# function to print list
-def Print_list(list):
+#include <iostream>
+using namespace std;
+int partition(int arr[], int start, int end){
+    int pivot = arr[end];
+    int p_index = start;
+    for(int i = start; i <= end - 1; i++){
+        if(arr[i] <= pivot){
+            swap(arr[i], arr[p_index]);
+            p_index++;
+            
+        }
+        
+    }
+    swap(arr[p_index], arr[end]);
+    return p_index;
+}
+void quick_sort(int arr[], int start, int end){
+    if(start < end){
+    int p_index = partition(arr, start, end);
+    quick_sort(arr, start, p_index - 1);
+    quick_sort(arr, p_index + 1, end);
+}}
+int main(void){
+    int arr[7] = {1, 4, 1, 2, 7, 5, 2}; 
+    quick_sort(arr, 0, 6);
+    for(int i = 0; i <= 6; i++){
+        cout << arr[i] << " ";
+    }
+}
